f3f550be64a259540493ed61623cc1896e20d27c
svenalpha <svalphaforsale@yahoo.com>
2023-04-16 14:54:46 +0700

    vp6 2nd push of 16/4/2023

--- dist/server/entry-server.mjs
+++ dist/server/entry-server.mjs
@@ -8,12 +8,12 @@ function render() {
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
       </a>
-      <h1>Hello Viiiiiite!</h1>
+      <h1>Hello Vite!</h1>
       <div class="card">
         <button id="counter" type="button"></button>
       </div>
       <p class="read-the-docs">
-        Click on the Vite looooooogo to learn more
+        Click on the Vite logo to learn more
       </p>
     </div>
   `;
