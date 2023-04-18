1e17a76fb4c4434c4a21f36cc5cd87884c4df66f
svenalpha <svalphaforsale@yahoo.com>
2023-04-18 16:20:35 +0700

    ?th 18/04/2023 16:15

--- src/entry-server.js
+++ src/entry-server.js
@@ -1,20 +1,21 @@
 import javascriptLogo from './javascript.svg'
+const { BASE_URL } = import.meta.env;
 
 export function render() {
   const html = `
     <div>
       <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
+        <img src="${BASE_URL}vite.svg" class="logo" alt="Vite logo" />
       </a>
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
       </a>
-      <h1>Hello Vite!AAAAAA</h1>
+      <h1>Hello Vite!BBBBBBB</h1>
       <div class="card">
         <button id="counter" type="button"></button>
       </div>
       <p class="read-the-docs">
-        Click on the ViteAAAA logo to learn more
+        Click on the ViteBBBBB logo to learn more
       </p>
     </div>
   `
