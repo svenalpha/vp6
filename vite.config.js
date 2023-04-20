e3b36c659212a51efebbbccda6c41ba194b7aaaa
svenalpha <svalphaforsale@yahoo.com>
2023-04-20 16:39:04 +0700

    3rd commit on 20/04/2023 16:38

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,19 @@ import  {defineConfig} from 'vite';
 //const root = resolve(__dirname,'vp6');
 //const outDir = resolve(__dirname,'dist');
 
-export default({
-    base: '/vp6/',
-    build: {
-      //outDir,
-      emptyOutDir: true,
-      rollupOptions: {
-        input: {    
+export default defineConfig({
+    base: '/vp6/',                  
+    build: {                     
+      //outDir,                            
+      emptyOutDir: true,                   
+      rollupOptions: {                                 
+        input: {                                 
                  main: resolve(__dirname,'index.html'),
                  about: resolve(__dirname, 'src/about/index.html'),
-               },   
+               },                       
                      },        
             },                                                                                   
-})
+                           })        
 
 
 
