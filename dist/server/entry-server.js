9356ab808b468965fb6d522e7c896bee6657445d
svenalpha <svalphaforsale@yahoo.com>
2023-04-24 14:31:01 +0700

    2nd commit to gh-pages 24/04/2023  14:27

--- dist/server/entry-server.js
+++ dist/server/entry-server.js
@@ -12,12 +12,12 @@ function render() {
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
       </a>
-      <h1>Hello Vite!BBBBfffBBB</h1>
+      <h1>Hello Vite!BBB preview ffBBB</h1>
       <div class="card">
         <button id="counter" type="button"></button>
       </div>
       <p class="read-the-docs">
-        Click on the ViteBBBBB logo to learn more
+        Click on the Vite logo (preview)  to learn more
       </p>
     </div>
   `;
