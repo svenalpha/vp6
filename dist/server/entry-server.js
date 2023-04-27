6222cb2b9b4aaf410bae8df052fe4f3262300b5d
svenalpha <svalphaforsale@yahoo.com>
2023-04-27 17:37:50 +0700

    3rd commit to gh-pages 24/04/2023  17:37

--- dist/server/entry-server.js
+++ dist/server/entry-server.js
@@ -1,5 +1,5 @@
-const javascriptLogo = "/assets/javascript-8dac5379.svg";
-const { BASE_URL } = { "BASE_URL": "/", "MODE": "production", "DEV": false, "PROD": true, "SSR": true };
+const javascriptLogo = "/vp6/assets/javascript-8dac5379.svg";
+const { BASE_URL } = { "BASE_URL": "/vp6/", "MODE": "production", "DEV": false, "PROD": true, "SSR": true };
 function render() {
   const html = `
     <div>
@@ -9,7 +9,7 @@ function render() {
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
       </a>
-      <h1>Hello Vite!BBB preview vp6 BBB</h1>
+      <h1>Hello Vite!BBB preview vp6  BBB</h1>
       <div class="card">
         <button id="counter" type="button"></button>
       </div>
