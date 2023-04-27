0991973e3ce2d20688d8afc1548a8d2b744c0dc9
svenalpha <svalphaforsale@yahoo.com>
2023-04-27 16:43:37 +0700

    2nd commit to gh-pages 27/04/2023  16:43

--- dist/server/entry-server.js
+++ dist/server/entry-server.js
@@ -9,7 +9,7 @@ function render() {
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
       </a>
-      <h1>Hello Vite!BBB dev ffBBB</h1>
+      <h1>Hello Vite!BBB preview vp6 BBB</h1>
       <div class="card">
         <button id="counter" type="button"></button>
       </div>
