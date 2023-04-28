c02fe486a6e9abb65822701a9d1349babee94e57
svenalpha <svalphaforsale@yahoo.com>
2023-04-28 15:15:04 +0700

    3rd the 4th  main  commit on 28/04/2023 15:14

--- src/entry-server.js
+++ src/entry-server.js
@@ -10,7 +10,7 @@ export function render() {
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
       </a>
-      <h1>Hello Vite!BBB dev vp6    BBB</h1>
+      <h1>Hello Vite!BBB dev  vp6    BBB</h1>
       <div class="card">
         <button id="counter" type="button"></button>
       </div>
